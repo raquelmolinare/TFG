8f339e759184ae38cac4e964b0d08f2103004c97
Raquel Molina <raquelmolinareche@gmail.com>
2022-11-06 17:08:28 +0100

    AlzCare-1 Init API project

---
+++ Alz-care-APP/alz-care-api/app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_smorest import Api
+
+from config.general_config import get_config
+
+
+def create_app():
+    app = Flask(__name__)
+
+    # Setup general config
+    app.config.from_object(get_config())
+
+    return app
+
+
+# Create the app
+app = create_app()
+api = Api(app)
+
+app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ Alz-care-APP/alz-care-api/config/general_config.py
@@ -0,0 +1,30 @@
+from functools import lru_cache
+
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    API_TITLE: str = 'Alz Care API'
+    API_BASE_NAME: str = '/api'
+    API_VERSION: str = '0.1.0'
+
+    OPENAPI_VERSION: str = '3.0.2'
+    OPENAPI_URL_PREFIX: str = 'apidocs'
+    OPENAPI_SWAGGER_UI_PATH: str = '/swagger'
+    OPENAPI_SWAGGER_UI_URL: str = 'https://cdn.jsdelivr.net/npm/swagger-ui-dist/'
+    OPENAPI_RAPIDOC_PATH: str = '/rapidoc'
+    OPENAPI_RAPIDOC_URL: str = 'https://unpkg.com/rapidoc/dist/rapidoc-min.js'
+    OPENAPI_REDOC_PATH: str = '/redoc'
+    OPENAPI_REDOC_URL: str = (
+        'https://cdn.jsdelivr.net/npm/redoc@next/bundles/redoc.standalone.js'
+    )
+
+    ENVIRONMENT: str = 'D'
+
+    class Config:
+        env_file = ".env"
+
+
+@lru_cache()
+def get_config():
+    return Settings()
